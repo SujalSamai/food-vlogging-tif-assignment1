3ac921d9788e3a009a1786d211ec0f6779c7e269
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2023-09-10 23:36:22 +0530

    Changed width in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ export const Footer = () => {
             <li>(904) 443-0343</li>
           </ul>
         </div>
-        <div className='w-1/6'>
+        <div className='lg:w-1/6'>
           <h3 className='font-source font-semibold text-xl text-blueish'>More</h3>
           <ul className='flex flex-col gap-4 mt-3 text-xs lg:text-sm'>
             <li><a href='#'>About Us</a></li>
